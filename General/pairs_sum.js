da0eb860ff99f438b08448f9f214df32bfaa7e6e
Michael Ho <michaelho3624@gmail.com>
2016-10-12 14:54:03 -0400

    Added a solution for pairs with sum to 10

---
+++ General/pairs_sum.js
@@ -0,0 +1,32 @@
+//without extreme cases
+function checkSum10(num_array) {
+    var map_freq = {};
+  
+  for (var i = 0; i < num_array.length ; i++)
+  {
+    var operand = num_array[i].toString();
+        
+    if (map_freq[operand] === undefined ) {
+        map_freq[operand] = 1;
+    }
+    else {
+        map_freq[operand]++;
+    }
+  }
+  
+  var twice_count = 0;
+  
+  for (var j = 0; j < num_array.length; j++)
+  {
+    var second_operand = (10 - num_array[j]).toString();
+    
+    twice_count+= map_freq[second_operand];
+    
+    if (10 - num_array[j] === num_array[j]) {
+        twice_count--;
+    }
+  }
+  
+  return twice_count/2
+ 
+}
